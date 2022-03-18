713e12269fbd5903a6a08ec59e1183394152e8b8
Eduardo Collazo <ecollazodominguez@gmail.com>
2022-03-18 15:36:10 +0100

    Agregado panel de carga y funciones adicionales

--- js/script.js
+++ js/script.js
@@ -4,20 +4,44 @@ import { creacionFichaDeVuelos } from "./dataVuelos.js";
 
 const buttonSwap = document.querySelector("button#cambio");
 const buttonSubmit = document.querySelector("#viajar");
+const { body } = document;
+
+const toggleDisableButton = (button) => {
+  button.toggleAttribute("disabled");
+};
+
+const resetInputs = () => {
+  vuelosForm.elements.origen.value = "";
+  vuelosForm.elements.destino.value = "";
+};
+
+const panelLoading = () => {
+  const sectionLoading = document.createElement("section");
+  sectionLoading.innerHTML = `<h1>Cargando vuelos...</h1>`;
+  sectionLoading.classList.add("loading");
+  body.prepend(sectionLoading);
+
+  return sectionLoading;
+};
+
+const removePanel = (panel) => {
+  panel.remove();
+};
 
 //Evento de cuando se hace el submit (Se le da al botón viajar);
 vuelosForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   if (e.submitter === buttonSubmit) {
+    toggleDisableButton(buttonSubmit);
+    const sectionLoading = panelLoading();
     try {
       await creacionFichaDeVuelos();
-
-      /* console.log(datos); */
     } catch (error) {
-      alert(error.message);
+      alert(error);
     }
-    vuelosForm.elements.origen.value = "";
-    vuelosForm.elements.destino.value = "";
+    removePanel(sectionLoading);
+    resetInputs();
+    toggleDisableButton(buttonSubmit);
   }
 });
 
